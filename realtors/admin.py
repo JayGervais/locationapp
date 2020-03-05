de0f762c03795e98800c245f3459222a0f79d998
Jay Gervais <mrjaygervais@gmail.com>
2020-03-05 06:36:28 -0700

    pull house listings from database

--- realtors/admin.py
+++ realtors/admin.py
@@ -3,4 +3,11 @@ from .models import Realtor
 
 
 # customize admin area
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
